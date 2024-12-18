ed697206697236f897f3a7c81c0449e330877c2d
nyacho04 <dnachod83@gmail.com>
2024-12-18 11:05:45 -0300

    display prompt for the shell, authors page, .h file and main file

---
+++ nashell.c
@@ -0,0 +1,45 @@
+#include "nashell.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+void display_prompt (void)
+{
+    printf("nashell$");
+    fflush(stdout);
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main (void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    while (1)
+    {
+        display_prompt();
+        read = getline(&line, &len, stdin);
+
+        if (read == -1)
+        {
+            break;
+        }
+        if (line[0] == '\n')
+        {
+            continue;
+        }
+        printf("funca %s", line);
+        free(line);
+        line = NULL;
+        len = 0;
+    }
+        return (0);
+}
---
+++ nashell.h
@@ -0,0 +1,7 @@
+#ifndef NASHELL_H
+#define NASHELL_H
+
+void display_prompt (void);
+int main (void)
+
+#endif
